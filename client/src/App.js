4709341489a163fcf605ef8f48a7e0825f3137c6
unknown <chrisitne.brown@bhcc.edu>
2021-02-08 20:06:24 -0500

    ui commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import React, {Components} from 'react';
-import './App.css';
 import {Link} from 'react-router-dom';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import Cafe from './components/Cafe';
 import Home from './components/Home';
 import Library from './components/Library';
@@ -16,7 +12,7 @@ export default class App extends Component {
                <div>
                    <Switch>
                        <Route exact path= "/" component= {Home} />
-                       <Route exact path= "/Home" component= {Home} />
+                       <Route exact path= "/home" component= {Home} />
                        <Route exact path="/cafe" component= {Cafe} />
                        <Route exact path = "/library" component= {Library} />
                    </Switch>
